c26acfa4195b3043aada9e6eb34a880c48bf7b81
Kush Shah <21DCS111@charusat.edu.in>
2023-07-25 22:20:46 +0530

    Prac2

--- prac2/src/App.js
+++ prac2/src/App.js
@@ -5,6 +5,8 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/index.js';
 import About from './pages/About.js';
 import Contact from './pages/Contact.js';
+import Services from './pages/Services.js';
+import Signup from './pages/Signup';
 
 
 
@@ -17,6 +19,8 @@ function App() {
         <Route path="/" element={<Home/>} />
         <Route path="/contact" element={<Contact/>} />
         <Route path="/about" element={<About/>} />
+        <Route path="/services" element={<Services/>} />
+        <Route path="/signup" element={<Signup/>} />
       </Routes>
     </Router>
     </>
